8ba81dfb0aa87696dc4b2981e84dc776f4bbbcbd
zodd1 <adilio.scbr@gmail.com>
2016-05-12 23:41:16 -0600

    avance

---
+++ app/src/main/java/fia/ues/sv/login/ConsultarUsuarioActivity.java
@@ -0,0 +1,13 @@
+package fia.ues.sv.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsultarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_usuario);
+    }
+}
--- app/src/main/java/fia/ues/sv/login/CrudUsuario.java
+++ app/src/main/java/fia/ues/sv/login/CrudUsuario.java
@@ -1,14 +1,43 @@
 package fia.ues.sv.login;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class CrudUsuario extends AppCompatActivity {
-
+public class CrudUsuario extends ListActivity {
+    String[] menu={"Nuevo Usuario","Actualizar Usuario","Eliminar Usuario", "Consultar Usuario" };
+    String[] activities ={"","CrearUsuarioActivity","ActualizarUsuarioActivity","EliminarUsuarioActivity","ConsultarUsuarioActivity"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crud_usuario);
+
+        View header = getLayoutInflater().inflate(R.layout.header_usuario, null);
+        ListView listView = getListView();
+        listView.addHeaderView(header);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.menu_row,R.id.label,menu);
+        setListAdapter(adapter);
+    }
+
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+
+        String nombreValue=activities[position];
+        System.out.println("------------------------------>"+nombreValue+" "+position);
+
+        try{
+            Class<?> clase=Class.forName("fia.ues.sv.login."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);
+
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
     }
 }
